3838e1ff11b0c7c2aa5f46d61dcbdefbfe2bf926
DESKTOP-I6GK69U\Usuario <gustavo_chermout@hotmail.com>
2019-10-02 00:14:26 -0300

    Criação da entidade User

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router();
 
-routes.post('/users/', (req, res) => {
-    return res.json(req.body);
-});
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
