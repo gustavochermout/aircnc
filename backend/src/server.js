202d7a4aba18a37926ea853de45e4dd441c81902
DESKTOP-I6GK69U\Usuario <gustavo_chermout@hotmail.com>
2019-10-01 23:55:21 -0300

    Criação de conexão com o MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({message: "Hello World"});
+mongoose.connect('mongodb+srv://omnistack:omnistack9@omnistack-dp3qf.mongodb.net/semana09?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+//req.query = Acessar query params (filtros)
+//req.params = Acessar route params (edição, delete)
+//req.body = Acessar corpo da requisição
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
