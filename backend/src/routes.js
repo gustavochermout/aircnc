ce014d9cdf08f111af42eaa8167aee28dffef02e
DESKTOP-I6GK69U\Usuario <gustavo_chermout@hotmail.com>
2019-10-02 22:54:27 -0300

    Criação da entidade Booking

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
 const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
@@ -14,5 +15,7 @@ routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
