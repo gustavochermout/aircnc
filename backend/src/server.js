70e0e2b8d7dfa9ff0278b8ec13e4c59275ee615e
DESKTOP-I6GK69U\Usuario <gustavo_chermout@hotmail.com>
2019-10-04 23:18:33 -0300

    Criação das páginas Login, Dashboard e New

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack9@omnistack-dp3qf.mongodb.net
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,17 @@
-import React, { useState } from 'react';
-import api from './services/api';
+import React from 'react';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(event){
-    event.preventDefault();
-
-    const response = await api.post('/sessions', { email });
-
-    console.log(response);
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnc"/>
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>  
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="Email">E-MAIL *</label>
-          <input 
-            type="email" 
-            id="email" 
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />  
       </div>    
     </div>  
   );
